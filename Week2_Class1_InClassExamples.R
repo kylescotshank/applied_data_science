e4c6566a2fe474b254eb401cc1895a7071f49cd7
Kyle Shank <kyle.s.shank@gmail.com>
2021-09-21 14:11:00 -0400

    Adding in our sick sweet in class file because we're awesome

--- Week2_Class1_InClassExamples.R
+++ Week2_Class1_InClassExamples.R
@@ -13,18 +13,28 @@
 # Let's Practice Arithmetic 
 # # ---------------------------------------------------------------
 
+# Addition = + 
 10 + 10 
+# Subtraction
 2 - 1 
+# Multiply
 3 * 6 
+# Divide
 2 / 7
+8 / 2  
+# YOU HAVE THE POWER! 
 2 ^ 5 
 2 ** 5 
+# 
 5 %% 2 
 2 %/% 7 
+7 %/% 2
 
 1 / 1
+1 ^ 0
 1 / 0
 0 / 0
+
 
 
 # All of the basic arithmetic operators 
@@ -36,12 +46,21 @@
 # x %% y	modulus (x mod y) 5%%2 is 1
 # x %/% y	integer division 5%/%2 is 2
 
+# R uses a specific order of operations - BEDMAS 
+
+2 * 8 
+3 * (2 * 8)
+3 / (3 / (3 / 8))
+
+()
 # # ---------------------------------------------------------------
 # Let's learn a couple of useful mathematical functions! 
 # # ---------------------------------------------------------------
 
 # Finds the sum of numbers or a series of numbers!
 sum(2,3)
+sum(1,2,3,4,5)
+sum(1:10)
 
 # Finds the absolute value of a number or series of numbers
 abs(-15)
@@ -60,6 +79,8 @@ log(10,10)
 # Finds the exponential of a function
 exp(2.3)
 
+?exp
+
 # Find the square root of a number!
 sqrt(9)
 
@@ -70,20 +91,34 @@ sqrt(9)
 # Mix and match!
 
 log(1:30, base = 2)
-sqrt(sum(log(1:30), base = 2))
+
+
+sqrt(
+  sum(
+  log(1:30), base = 2))
 
 # # ---------------------------------------------------------------
 # Let's Talk about Logic! 
 # # ---------------------------------------------------------------
 
 3 < 4 
+
 5 <= 6
+
 4 > 12 
+
 3 >= 3
+
 2 == 1 
+
 2 == 2 
+
+'Kyle' == 'Kyle'
+
 2 = 2
+
 2 != 4
+
 2+1i - 3+0i
 
 # All of the basic logic operators 
@@ -107,7 +142,11 @@ sqrt(sum(log(1:30), base = 2))
 
 my_age <- 35
 # This is a variable - which is also a vector of length one! 
+
+gray = 68
+
 my_age 
+
 my_kids_ages <- c(4,7) 
 # This is a vector - of length two!
 my_kids_ages
@@ -115,12 +154,15 @@ my_kids_ages
 
 c("Willa","Gideon") -> my_kids_names
 my_kids_names
+
 str(my_kids_names)
 
 my_age * my_kids_ages 
 my_age + my_kids_ages
 
 my_age^2
+
+my_kids_ages * my_kids_ages
 
 my_kids_names + my_kids_ages
 
@@ -165,7 +207,9 @@ kyle <- list(age = 35,
 kyle
 class(kyle)
 kyle$age
+kyle$children
 class(kyle$age)
+class(kyle$nerd)
 
 # Data Frames: The real workhorses of R!
 
@@ -177,6 +221,8 @@ str(class_results)
 
 class_results$age
 
+class_results$age[1]
+
 names(class_results)
 
 
@@ -187,6 +233,7 @@ names(class_results)
 my_kids_names + my_kids_ages
 
 my_kids <- c(my_kids_names,my_kids_ages)
+my_kids
 # This is *implicit* coercion! 
 str(my_kids)
 
@@ -199,6 +246,9 @@ str(my_kids)
 number_list <- 0:6
 number_list
 (as.logical(number_list))
+as.character(number_list)
+
+
 
 # # ---------------------------------------------------------------
 # Let's go back to data frames and such ... 
@@ -208,7 +258,14 @@ class_results
 
 new_kid <- c(NA,22)
 
+another_new_kid <- c(68, "NA")
+
 class_results <- rbind(class_results, new_kid)
+
+class_results <- rbind(class_results, another_new_kid)
+
+# rbind is BINDING ROWS 
+# cbind is BINDING COLUMNS 
 
 class_results
 
@@ -216,8 +273,8 @@ is.na(class_results)
 
 sum(is.na(class_results))
 
-sum(!complete.cases(class_results))
-sum(complete.cases(class_results))
+complete.cases(class_results)
 
 
+is.na(class_results$age)
 
